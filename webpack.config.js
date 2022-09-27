51b8f8195f8677e08d1ae0182979afa9a5354705
Edozie <edoziechukwuma@gmail.com>
2022-09-27 14:10:51 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,9 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
   optimization: {
     runtimeChunk: "single",
